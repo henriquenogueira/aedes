bea93214242a436079eb2ccd49cb7978fedfbd5b
Henrique Nogueira <hrqnogueira@gmail.com>
2016-03-26 21:18:04 -0300

    Added tests to system checks

---
+++ aedes_server/core/tests/test_system_checks.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.test import TestCase
+from ..apps import CoreConfig
+from ..checks import check_threshold
+
+
+class SystemCheckTest(TestCase):
+    def setUp(self):
+        delattr(settings, 'THRESHOLD')
+        self.errors = check_threshold(CoreConfig)
+
+    def test_threshold_missing(self):
+        '''Error list should not be empty'''
+        self.assertNotEqual(0, len(self.errors))
+
+    def test_treshold_error_code(self):
+        '''Error code must be e_001.'''
+        self.assertEqual('aedes.e_001', self.errors[0].id)
+
+    def tearDown(self):
+        setattr(settings, 'THRESHOLD', 0.0005)
