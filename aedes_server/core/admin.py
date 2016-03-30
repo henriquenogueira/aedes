489ee4549c11cbf2fae331112be16f27be24579e
Henrique Nogueira <hrqnogueira@gmail.com>
2016-03-29 22:09:49 -0300

    Changed photo field to blank=True and added to admin UI.

--- aedes_server/core/admin.py
+++ aedes_server/core/admin.py
@@ -3,9 +3,16 @@ from .models import Report, Cluster
 
 
 class ReportModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'device_id', 'latitude', 'longitude', 'category', 'reported_at')
+    list_display = ('id', 'device_id', 'latitude', 'longitude',
+                    'category', 'reported_at', 'has_photo')
     list_filter = ('category',)
 
+    def has_photo(self, obj):
+        print(obj.photo)
+        return obj.photo != ''
+
+    has_photo.short_description = 'tem foto?'
+    has_photo.boolean = True
 
 class ClusterModelAdmin(admin.ModelAdmin):
     list_display = ('label', 'latitude', 'longitude', 'breeding_count', 'focus_count', 'suspicion_count')
--- aedes_server/core/models.py
+++ aedes_server/core/models.py
@@ -16,7 +16,7 @@ class Report(models.Model):
 
     latitude = models.FloatField('latitude')
     longitude = models.FloatField('longitude')
-    photo = models.ImageField('foto', upload_to='/upload/%Y/%m/%d/', null=True)
+    photo = models.ImageField('foto', upload_to='/upload/%Y/%m/%d/', blank=True)
     device_id = models.CharField('ID do aparelho', max_length=255)
     category = models.CharField('categoria', max_length=1, choices=REPORT_CATEGORIES)
     reported_at = models.DateTimeField('reportado em', auto_now_add=True)
